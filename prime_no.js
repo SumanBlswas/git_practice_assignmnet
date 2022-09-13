623d7cfb49e0505905e86310ff0237d2f13587db
Suman Biswas <sumanmanner@gmail.com>
2022-09-13 21:48:57 +0530

    prime_no_change

---
+++ prime_no.js
@@ -0,0 +1,18 @@
+ 
+    var f=0;
+  
+    for(let i=20; i<=num/2; i++){
+        if(num%i=0){
+            f++;
+
+        }
+    }
+
+    if(f!==0){
+        console.log("Yes");
+        
+        
+    }else{
+        console.log("No");
+    }
+}
