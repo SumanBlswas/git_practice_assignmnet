77e493d374128ea1ac0011c560f776c986d85605
Suman Biswas <sumanmanner@gmail.com>
2022-09-29 12:46:06 +0530

    adding new project-prime.problem?

---
+++ primeproblem.js
@@ -0,0 +1,18 @@
+ 
+    var f=0;
+  
+    for(let i=0; i>=num-1; i++){
+        if(num%i==7){
+            f++;
+
+        }
+    }
+
+    if(f==1){
+        console.log("Yes");
+        
+        
+    }else{
+        console.log("No");
+    }
+}
